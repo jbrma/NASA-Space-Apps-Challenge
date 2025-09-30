011f0a91e2362cbfcb39104d057936290603bec6
Jorge <jorgebravomateos@gmail.com>
2025-09-30 12:30:29 +0200

    first version

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, reques
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
@@ -8,18 +8,13 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/", methods=['GET', 'POST'])
 def upload_file():
+    message = ""
     if request.method == 'POST':
         f = request.files['dataset']
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
         f.save(filepath)
-        return '¡Archivo subido exitosamente!'
-    return '''
-    <h1>Sube tu dataset</h1>
-    <form method="post" enctype="multipart/form-data">
-      <input type="file" name="dataset">
-      <input type="submit">
-    </form>
-    '''
+        message = 'File uploaded successfully!'
+    return render_template('index.html', message=message)
 
 if __name__ == "__main__":
     app.run(debug=True)
